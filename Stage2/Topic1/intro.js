38c7ad16b8d53d55b76fe5cf80cd39c19a127d34
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-11-10 22:09:36 +0400

    js lessons

---
+++ Stage2/Topic1/intro.js
@@ -0,0 +1,25 @@
+// console.log("Hello, JavaScript!");
+
+
+// Primitive & InPrimitive
+
+// NUM
+var age = 25; // 1.NUM / INT=25 FLOAT=12.5
+var allSalary = 12501.2; // 1.NUM / INT=25 FLOAT=12.5
+var workingMonth = 14
+
+//STRING
+var fullname = "John Doe"
+var fullname2 = 'John Doe 2'
+var fullname3 = `John Doe 2`
+
+// BOOLEAN
+var isNew = false
+var isWorking = true
+
+// NULL
+var productImg = "https://kontakt.az/mebeller/divan.png"
+var product2Img = null
+
+//Undefined
+var password = undefined
---
+++ Stage2/Topic1/operators.js
@@ -0,0 +1,89 @@
+//Arithmetic Operators
+
+// var a = 20
+// var b = 30
+
+// var c = a * b // 50 
+
+// console.log(c);
+
+
+// var a = 2
+
+// a = a / 10
+
+// console.log(a);
+
+// var age = 4
+
+// // var result = age % 2
+// var result = age ** 2
+
+// console.log(result);
+
+// var result = 100 * (2 + 10)
+
+// console.log(result);
+
+// var num = 6
+
+// num = num + 1
+
+// INCREMENT
+// num++
+
+// DECREMENT
+// num--
+
+// console.log(num);
+
+
+
+// var appointmentPrice = 200
+// var employeePersentage = 30
+
+// var employeeProfit = (appointmentPrice * employeePersentage) / 100
+
+// var branchProfit = appointmentPrice - employeeProfit
+
+// console.log("Isci maasi",employeeProfit);
+// console.log("Filial qazanci",branchProfit);
+
+
+
+
+
+
+// Assignment Operators
+// var a = 5
+
+// // a = a + 10
+// // a = a - 10
+// // a = a * 10
+// // a = a / 10
+// // a = a ** 10
+
+// a += 10
+// a -= 10
+// a *= 10
+// a /= 10
+// a **= 10
+
+// console.log(a); // 15
+
+
+// Comparison Operators
+var personalAge = 6;
+var accessAge = 5
+
+// var result = x > 18 // false
+// var result = x > y // true
+// var resultAsef = x == y // true
+// var resultAsef = x === y // true
+// var resultAsef = x === y // true
+// var resultAsef = x != y // false
+// var resultAsef = x !== y // false
+// var result = personalAge >= accessAge// false
+var result = personalAge <= accessAge// false
+
+console.log(result);
