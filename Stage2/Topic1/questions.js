a3ae5f0dbe5a87b3d947687cb54b551535b70ca1
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-11-17 21:39:55 +0400

    Add basic game logic with questions and scoring system

---
+++ Stage2/Topic1/questions.js
@@ -0,0 +1,42 @@
+
+
+
+var isStart = confirm("Do you want to start game?")
+var point = 0
+
+console.log("isStart",isStart);
+
+
+if(isStart){
+
+    //Q1
+    var q1 = prompt("2 + 2 = ?") // 4
+
+    // if(parseInt(q1) === 4){
+    if(q1 == 4){
+        point = point + 10
+        
+        alert(`Congratulations!Your point is ${point} `)
+        //...
+
+        //Q2
+        var q2 = prompt("100 / 3 = ?") // 4
+
+        if(q2 == "33.3" || q2  == "33.33"  || q2  == "33.333" || q2  == "33.3333" ){
+            point= point + 10
+            alert(`Congratulations!Your point is ${point} `)
+
+            //....
+
+        }else{
+            alert("Incorrect answer!")
+        }
+
+    }else{
+        alert("Incorrect answer!")
+    }
+
+
+}else{
+    console.log("See you again!");
+}
