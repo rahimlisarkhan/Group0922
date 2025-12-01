b90762993f541af4b80d8cd33af938616a6fe7c1
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-12-01 22:01:01 +0400

    Refactor script references and add foundational functions for Topic 3

---
+++ Stage2/Topic3/codewars.js
@@ -0,0 +1,122 @@
+//CODEWARS
+
+
+// //SOLITION 1
+//     Test.assertEquals(countWords(" Arthur "), 1);
+//     Test.assertEquals(countWords(" David"), 1);
+//     Test.assertEquals(countWords("Nelson "), 1);
+//     Test.assertEquals(countWords("  Hello Gomer  "), 2);
+//     Test.assertEquals(countWords("  Hello     Bart  "), 2);
+
+
+
+// function countWords(str) {
+
+//     let newStr = str.trim();
+
+//     let arr = newStr.split(/\s+/);
+
+//     console.log(arr);
+    
+
+//     // let count = arr.length
+//     let count = 0
+
+//     for (let i = 0; i < arr.length; i++){
+
+//         if(arr[i] !== ""){
+//             count++;
+//         }
+
+//     }
+
+//     return count;
+  
+// }
+
+
+// // countWords("  Hello     Bart  ")
+// // countWords(" Arthur ")
+// // countWords(" David")
+// // countWords("Nelson ")
+// // countWords("  Hello Gomer  ")
+// countWords("﻿Hello﻿World ")
+
+
+
+// //SOLITION 1
+    // assert.strictEqual(solution('world'), 'dlrow');
+    // assert.strictEqual(solution('hello'), 'olleh');
+    // assert.strictEqual(solution(''), '');
+    // assert.strictEqual(solution('h'), 'h');
+
+
+//     function solution(str){
+//         let result = str.split("") // convert string to array
+//         let result2 = result.reverse() // reverse array
+//         let result3 = result2.join("") // convert array to string
+
+//         console.log("result3",result3);
+        
+
+//         return result3;
+// }
+
+
+// solution('world')
+// solution("hello")
+// solution('')
+// solution('h')
+
+
+// //SOLITION 3
+//   assert.strictEqual(howMuchILoveYou(7),"I love you")
+//   assert.strictEqual(howMuchILoveYou(3),"a lot")
+//   assert.strictEqual(howMuchILoveYou(6),"not at all")
+
+    // function howMuchILoveYou(nbPetals) { 
+    //     let phrases = [
+    //         "I love you",
+    //         "a little",
+    //         "a lot",
+    //         "passionately",
+    //         "madly",
+    //         "not at all"
+    //     ];
+
+    //     let index = (nbPetals - 1) % phrases.length;
+
+    //     console.log("index", phrases[index]);
+
+    //     return phrases[index];
+    // }
+
+
+    // howMuchILoveYou(7)
+
+
+// //SOLITION 4
+
+    // (uefaEuro2016(['Germany', 'Ukraine'], [2, 0]), "At match Germany - Ukraine, Germany won!");
+    // assert.strictEqual(uefaEuro2016(['Belgium', 'Italy'], [0, 2]), "At match Belgium - Italy, Italy won!");
+    // assert.strictEqual(uefaEuro2016(['Portugal', 'Iceland'], [1, 1]), "At match Portugal - Iceland, teams played draw.");
+
+function uefaEuro2016(teams, scores){
+    let team1 = teams[0];
+    let team2 = teams[1];
+
+    let score1 = scores[0];
+    let score2 = scores[1];
+
+
+    if(score1 > score2){
+        return `At match ${team1} - ${team2}, ${team1} won!`;
+    } else if (score2 > score1){
+        return `At match ${team1} - ${team2}, ${team2} won!`;
+    } else {
+        return `At match ${team1} - ${team2}, teams played draw.`;
+    }
+
+}
+
+uefaEuro2016(['Germany', 'Ukraine'], [2, 0])
---
+++ Stage2/Topic3/function.js
@@ -0,0 +1,101 @@
+// Function
+
+// function A(){
+//     let age = 25;
+//     console.log("Function A",age);
+
+//     // your logic here
+// }
+
+
+// A();
+
+
+
+// function sumCalculator(num1, num2, num3 = 0){
+//     let sum = num1 + num2 + num3;
+//     console.log("Sum is:", sum);
+// }
+
+// function multiblerCalculator(num1, num2){
+//     let multiply = num1 * num2;
+//     console.log("Multiply is:", multiply);
+// }
+
+// sumCalculator(5, 10, 100);
+// sumCalculator(20, 30);
+// sumCalculator(100, 200);
+
+// multiblerCalculator(5, 10);
+// multiblerCalculator(3, 7);
+// multiblerCalculator(4, 6);
+
+
+// function sumCalculator(num1, num2, num3 = 0){
+//     let sum = num1 + num2 + num3;
+
+//     return sum;
+// }
+
+
+// let result = sumCalculator(5, 10, 15);
+
+// console.log("Result is:", result); // 30
+
+
+// function greetUser(name,balance){
+
+//     let result = `Hello, ${name}! Welcome to the platform. Your balance is $${balance}.`
+
+//     return result;
+// }
+
+// let message = greetUser("Alice", 100);
+// let message2 = greetUser("Bob", 250);
+// let message3 = greetUser("Charlie", 300);
+
+// console.log(message); // Output: Hello, Alice! Welcome to the platform. Your balance is $100.
+// console.log(message2); // Output: Hello, Bob! Welcome to the platform. Your balance is $250.
+// console.log(message3); // Output: Hello, Charlie! Welcome to the platform. Your balance is $300.
+
+let fruits = ["apple", "banana", "cherry", "date"];
+// let numbers = [10, 20, 30, 40, 50];
+let students = ["Alice", "Bob", "Charlie", "David","Leman"];
+
+
+// for (let i = 0; i < fruits.length; i++){
+//     console.log("Fruit:", fruits[i]);
+// }
+
+// for (let j = 0; j < numbers.length; j++){
+//     console.log("Number:", numbers[j]);
+// }
+
+// for (let k = 0; k < students.length; k++){
+//     console.log("Student:", students[k]);
+// }
+
+
+// function printArrayEl(strArray, char){
+//     let resultArray = [];
+
+//     for (let i = 0; i < strArray.length; i++){
+
+//         let el = strArray[i]
+//         let charLower = char.toLowerCase();
+
+//         if(el.toLowerCase().includes(charLower)){
+//             resultArray.push(el);
+//         }
+
+//     }
+
+//     return resultArray;
+// }
+
+// let result1 =  printArrayEl(fruits,"d")
+// let result2 = printArrayEl(students,"d")
+
+// console.log("Result 1:", result1);
+// console.log("Result 2:", result2);
+
