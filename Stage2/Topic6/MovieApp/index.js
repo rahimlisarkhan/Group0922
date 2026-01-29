4bd3364c6e00a150ed3227da52c4af04ac84d035
rahimlisarkhan <rahimlisarkhan@gmail.com>
2026-01-29 22:03:14 +0400

    Add loading indicator and refactor movie fetching logic in Movie App

--- Stage2/Topic6/MovieApp/index.js
+++ Stage2/Topic6/MovieApp/index.js
@@ -1,8 +1,15 @@
+"use strict"
+
 const movileList = document.querySelector(".movileList");
 const searchButton = document.querySelector("#searchButton");
 const searchInput = document.querySelector("#searchInput");
+const loadingIndicator = document.querySelector(".loading");
 
-searchButton.addEventListener("click", getMovies)
+
+// DRY - Don't Repeat Yourself
+searchButton.addEventListener("click", function(){
+    getMovies()
+})
 
 searchInput.addEventListener("keyup", (event) => {
     if(event.key === "Enter") {
@@ -10,25 +17,51 @@ searchInput.addEventListener("keyup", (event) => {
     }
 })
 
-function getMovies(){
+async function getMovies(){
+
+    try{
+
        const url = `http://www.omdbapi.com/?apikey=5addd3ac&s=${searchInput.value}`
+    
+       loadingIndicator.style.display = "block";
        
-       const promise = fetch(url)
+       const response = await fetch(url) // A
 
-            promise.then((response) => {
-                return response.json();
-            }).then((data) => {
-                renderMovies(data.Search);
-                
-                // console.log("Movie data:", data);
-            }).catch((err) => {
-                console.log("Promise rejected:", err);
-            }).finally(() => {
-                console.log("Promise finally");
-        })
 
-     searchInput.value = ""
+
+       const data = await response.json()
+
+        renderMovies(data.Search); //B
+
+        searchInput.value = ""
+
+    }catch(err){
+        console.log("Promise rejected:", err);
+    }finally{
+            loadingIndicator.style.display = "none";
+    }
 }
+
+// function getMovies(){
+//        const url = `http://www.omdbapi.com/?apikey=5addd3ac&s=${searchInput.value}`
+    
+//        loadingIndicator.style.display = "block";
+       
+//        const promise = fetch(url) // A
+    
+//             promise.then((response) => {
+//                 return response.json();
+//             }).then((data) => {
+//                 renderMovies(data.Search); //B
+//             }).catch((err) => {
+//                 console.log("Promise rejected:", err);
+//             }).finally(() => {
+//                 console.log("Promise finally");
+//                 loadingIndicator.style.display = "none";
+//         })
+
+//      searchInput.value = ""
+// }
 
 function renderMovies(movieData) {
 
@@ -66,3 +99,63 @@ function renderMovies(movieData) {
 // const age =  18
 
 // const result = `John is ${age}` 
+
+
+// try{
+//     const a = 10;
+
+//     a = 20
+
+//     console.log(a);
+// }catch(error){
+//     console.log(error);
+//     alert(error)
+// }
+
+
+
+
+
+// let b = 15;
+
+// console.log(b);
+
+
+
+// async function getBlogs(){
+
+//     try{
+
+//         const result = await A(2,3)
+
+//         B(result)
+
+//     }catch(error){
+//         console.log(error);
+//     }finally{
+//         console.log("Finally block");
+//     }
+
+// }
+
+
+// const promise = navigator.getBattery()
+
+// promise.then((battery)=>{
+//     console.log("Battery data:",battery);
+// }).catch((err)=>{
+//     console.log("Error fetching battery data:",err);
+// })
+
+
+ async function batteryLevel(){
+    try{
+        const battery = await navigator.getBattery()
+
+        console.log("Battery data:",battery);
+    }catch(err){
+        document.querySelector(".toast-body").innerText = err
+    }
+}
+
+// batteryLevel()
